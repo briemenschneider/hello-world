696539a41077e9b55005568897a930d803a2a9c7
Brice Riemenschneider <brice.riemenschneider@gmail.com>
2018-08-18 15:28:57 +0200

    added lots of functionality

--- hello.c
+++ hello.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-boolean isPrime(int n) {
+bool isPrime(int n) {
   int i;
+
   for (i = 2; i < n; i++) {
-    if (n mod i == 0) {
+    if (n % i == 0) {
       return false;
     }
-    else i++;
   }
   return true;
 }
 
 int main (void) {
   int j;
-  boolean ans;
-  
+  bool ans;
+
+  //standard greeting plus question
   printf("hello world.\n");
   printf("Enter your suggested prime: ");
-  scanf("%d", &j);
-  ans = isPrime(j);
-  if (ans) {
-    printf("Your integer %d is Prime.\n", j);
+  scanf("%d", &j);  //take a number, any number
+  
+  //now check against invalid numbers
+  if (j <= 1) {
+    printf("You've entered a value less than 2, and is therefor invalid.\n");
+    return EXIT_SUCCESS;
   }
-  else printf("Your integer %d is not Prime.\n", j);
+  else {
+    ans = isPrime(j);
+    if (ans) {
+      printf("Your integer %d is Prime.\n", j);
+    }
+    else printf("Your integer %d is not Prime.\n", j);
+  }
   
   return EXIT_SUCCESS;
 }
