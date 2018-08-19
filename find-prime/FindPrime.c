3a5f0288ef42cffb75333d163ce7521a8d6af181
Brice Riemenschneider <brice.riemenschneider@gmail.com>
2018-08-19 15:17:03 +0200

    New program that is efficient at finding primes up to a certain point

---
+++ find-prime/FindPrime.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+//Simple recursive function to test for prime by testing against factors 
+bool isPrime(int n, int primestore[], int index) {
+  int i;
+
+  for (i = 0; i < index; i++) {
+    if (n % primestore[i] == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//function to find primes between two values and returns the number of Primes found
+void findPrimes(int min, int max, int primestore[], int *index) {
+  int i = 0;
+
+  if (min > max) {
+    printf ("Error: Unexpected min greater than max.\n");
+    return;
+  }
+  else if (max < 2) {
+    printf("Error: unexpected max < 2.\n");
+    return;
+  }
+  else if (max == 2) { //lowest prime
+    printf("Error at max is 2.\n");
+    return;
+  }
+  else if (max > min) { //split the array in half, doing the lower half first
+    i = min + (max-min)/2;
+    findPrimes(min, i, primestore, index);
+    findPrimes(i+1, max, primestore, index);
+  }
+  else if (max == min) {
+      if (primestore[i] <= 1) {
+	printf ("Possible error: reached end of prime store before expected or had bad value.");
+	return;
+	}
+      else if (isPrime(max, primestore, *index)) {
+	primestore[*index] = max;
+	*index = *index + 1;
+      }
+  }
+  else {
+    printf("Error: Something strange happened. Max and min have no relativistic value.\n");
+  }
+  return;
+}
+	
+	
+      
+    
+    
+
+bool fastPrime(int n) {
+  int knownprimes[16000];
+  int index = 0;
+  int i;
+  int j;
+
+  //do the quick checks
+  if (n <= 1) {
+    return false;
+  }
+  else if (n == 2) {
+    return true;
+  }
+  else if (n % 2 == 0) { //get the even numbers out of the way
+    return false;
+  }
+  
+  //initialize the array
+  for (i = 0; i < 16000; i++) {
+    knownprimes[i] = 0;
+  }
+  //add the first two primes - gimmes
+  knownprimes[0] = 2;
+  index = 1; //set the marker to the end of the array
+
+  j = (n / 2) + (n % 2); //carve the field in half
+  findPrimes(3, j, knownprimes, &index);
+  printf("Found %d Primes to halfway point.\n", index);
+
+  if (isPrime(n, knownprimes, index)) {
+    return true;
+  }
+  else return false;
+}
+    
+
+int main (void) {
+  int j;
+  bool ans;
+
+  //Ask for the integer that we will check for prime
+  printf("Enter your suggested prime between 0-32000: ");
+  scanf("%d", &j);  //take a number
+  
+  //now check against invalid numbers
+  if (j <= 1) {
+    printf("You've entered a value less than 2, and is therefor invalid.\n");
+    return EXIT_SUCCESS;
+  }
+  else if (j > 32000) {
+    printf("You've entered a value that is too large.\n");
+    return EXIT_SUCCESS;
+  }
+  else {
+    ans = fastPrime(j);
+    if (ans) {
+      printf("Your integer %d is Prime.\n", j);
+    }
+    else printf("Your integer %d is not Prime.\n", j);
+  }
+
+  //end program
+  return EXIT_SUCCESS;
+}
